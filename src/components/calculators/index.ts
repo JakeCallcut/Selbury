9c27ac490f538976bb31db09a9c23c2857af85bd
Jake Callcut <jakecallcut123@gmail.com>
2026-03-08 09:53:42 +0000

    fixing deployment error

--- src/components/calculators/index.ts
+++
@@ -1,5 +0,0 @@
-export { MortgageCalculator } from './MortgageCalculator';
-export { StampDutyCalculator } from './StampDutyCalculator';
-export { PensionCalculator } from './PensionCalculator';
-export { BudgetPlanner } from './BudgetPlanner';
-export { GlossarySearch } from './GlossarySearch';
